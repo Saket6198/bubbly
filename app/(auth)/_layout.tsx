59ae71af940a3f3bf69358c2f3b13e5b2eb7041a
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-11-03 23:53:46 +0530

    updated configs

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,7 +2,11 @@ import { Stack } from "expo-router";
 import React from "react";
 
 const AuthLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return <Stack screenOptions={{ headerShown: false }}>
+    <Stack.Screen name="welcome" />
+    <Stack.Screen name="login" />
+    <Stack.Screen name="register" />
+  </Stack>;
 };
 
 export default AuthLayout;
